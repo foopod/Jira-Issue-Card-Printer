aae1a14d58a9a5aa9a42ec4f1165957aa151e379
Jono Shields <jonathonshields@gmail.com>
2017-04-19 13:36:06 +1200

    Update main.js

--- source-files/main.js
+++ source-files/main.js
@@ -229,25 +229,25 @@ var getIconStyle = function(type) {
     switch (type.toLowerCase()) {
         case 'default':
             style.color = 'SILVER';
-            style.image = 'https://jira.hashbrown.club/Jira-Issue-Card-Printer/resources/icons/objects.png';
+            style.image = 'https://jira.hashbrown.club/resources/icons/objects.png';
             style.size = '63%';
             break;
         case 'story':
         case 'user story':
             style.color = 'GOLD';
-            style.image = 'https://jira.hashbrown.club/Jira-Issue-Card-Printer/resources/icons/Bulb.png';
+            style.image = 'https://jira.hashbrown.club/resources/icons/Bulb.png';
             style.size = '63%';
             break;
         case 'bug':
         case 'problem':
         case 'correction':
             style.color = 'CRIMSON';
-            style.image = 'https://jira.hashbrown.club/Jira-Issue-Card-Printer/resources/icons/Bug.png';
+            style.image = 'https://jira.hashbrown.club/resources/icons/Bug.png';
             style.size = '63%';
             break;
         case 'epic':
             style.color = 'ROYALBLUE';
-            style.image = 'https://jira.hashbrown.club/Jira-Issue-Card-Printer/resources/icons/Flash.png';
+            style.image = 'https://jira.hashbrown.club/resources/icons/Flash.png';
             style.size = '63%';
             break;
         case 'task':
@@ -257,28 +257,28 @@ var getIconStyle = function(type) {
         case 'unteraufgabe':
         case 'technische aufgabe':
             style.color = 'WHEAT';
-            style.image = 'https://jira.hashbrown.club/Jira-Issue-Card-Printer/resources/icons/Task.png';
+            style.image = 'https://jira.hashbrown.club/resources/icons/Task.png';
             style.size = '63%';
             break;
         case 'new feature':
             style.color = 'LIMEGREEN';
-            style.image = "https://jira.hashbrown.club/Jira-Issue-Card-Printer/resources/icons/Plus.png";
+            style.image = "https://jira.hashbrown.club/resources/icons/Plus.png";
             style.size = '63%';
             break;
         case 'improvement':
         case 'verbesserung':
             style.color = 'CORNFLOWERBLUE';
-            style.image = 'https://jira.hashbrown.club/Jira-Issue-Card-Printer/resources/icons/Arrow.png';
+            style.image = 'https://jira.hashbrown.club/resources/icons/Arrow.png';
             style.size = '63%';
             break;
         case 'research':
             style.color = 'MEDIUMTURQUOISE';
-            style.image = 'https://jira.hashbrown.club/Jira-Issue-Card-Printer/resources/icons/ErlenmeyerFlask.png';
+            style.image = 'https://jira.hashbrown.club/resources/icons/ErlenmeyerFlask.png';
             style.size = '63%';
             break;
         case 'test':
             style.color = 'ORANGE';
-            style.image = 'https://jira.hashbrown.club/Jira-Issue-Card-Printer/resources/icons/CrashDummy.png';
+            style.image = 'https://jira.hashbrown.club/resources/icons/CrashDummy.png';
             style.size = '63%';
             break;
     }
