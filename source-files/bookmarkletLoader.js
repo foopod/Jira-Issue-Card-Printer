7c517797346614c15847369816f96fcc49301b04
Jono Shields <jonathonshields@gmail.com>
2017-04-19 13:34:53 +1200

    Update bookmarkletLoader.js

--- source-files/bookmarkletLoader.js
+++ source-files/bookmarkletLoader.js
@@ -1,7 +1,7 @@
 (function(){
         var head = document.getElementsByTagName('head')[0];
         var scriptElement = document.createElement('script');
-        scriptElement.src = 'https://jira.hashbrown.club/Jira-Issue-Card-Printer/bookmarklet.js';
+        scriptElement.src = 'https://jira.hashbrown.club/bookmarklet.js';
         head.appendChild(scriptElement);
         head.removeChild(scriptElement);
 })();
